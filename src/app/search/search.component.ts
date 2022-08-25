f99f91b13e541ab3ebdf00a6a333a4926a042d47
Baboula <91437082+Baboula@users.noreply.github.com>
2022-08-24 21:08:26 -0400

    ajout du formulaire de recherche

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
@@ -10,6 +11,11 @@ export class SearchComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onSubmit(form: NgForm){
+    console.log(form
+      .value)
+
   }
 
 }
