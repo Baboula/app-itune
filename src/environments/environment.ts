5d1cec8ef0e5adc1a27a9b12f2e2293b44a390ff
Baboula <91437082+Baboula@users.noreply.github.com>
2022-08-24 21:14:40 -0400

    ajout de l'apiEndpoint dans le fichier d'env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // for calling this link anywhere in application
+  apiEndpoint: 'https://itunes.apple.com/'
 };
 
 /*
